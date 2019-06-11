5c91082a560304f4697cf0c05d75f70112589158
Pierre Lambert <supagoof924@hotmail.com>
2019-06-11 23:48:34 +0200

    [ CONFIG ] New user now storing into database

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleID: String
+  googleID: String,
+  name : String,
+  mail : String
 });
 
 mongoose.model('users', userSchema)
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,11 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('ACCESSTOKEN : ', accessToken);
-      console.log('REFRESHTOKEN : ', refreshToken);
-      console.log('PROFILE : ', profile);
+      new User({
+        googleID: profile.id,
+        name: profile.name.givenName,
+        mail: profile.emails[0].value
+      }).save();
     }
   )
 );
