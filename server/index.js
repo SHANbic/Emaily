d8a564e855e5716b5027eda2669f0c633d626b2f
Pierre Lambert <supagoof924@hotmail.com>
2019-06-11 12:12:50 +0200

    [ DEPLOY ] Got the server prepared for a hereoky deployment

--- server/index.js
+++ server/index.js
@@ -5,5 +5,6 @@ app.get('/', (req, res) => {
   res.send({ working: true });
 });
 
-app.listen(5000)
-console.log('listening on http://localhost:5000')
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log('listening on http://localhost:' + PORT);
