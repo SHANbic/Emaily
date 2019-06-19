b4a4a099479e25dcf48429348e0f915172fae8d7
Pierre Lambert <supagoof924@hotmail.com>
2019-06-19 14:28:26 +0200

    [ FEATURE ] Updated Landing component

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center', fontWeight: 'bold' }}>
+      <h1 style={{ textShadow: '2px 10px 1px rgba(238, 110, 115,.2)' }}>
+        Emaily!
+      </h1>
+      <p>Collect feedback from your users</p>
+    </div>
+  );
+};
+
+export default Landing;
