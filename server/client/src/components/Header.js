fa5e95cd3ecba09ab9eea923e86f561ba0407f1d
Pierre Lambert <supagoof924@hotmail.com>
2019-06-19 13:45:03 +0200

    [ FEATURE ] Added google login to header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return '';
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Emaily</a>
           <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
+            <li>{this.renderContent()}</li>
           </ul>
         </div>
       </nav>
@@ -17,4 +30,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return { auth };
+};
+export default connect(
+  mapStateToProps,
+  null
+)(Header);
