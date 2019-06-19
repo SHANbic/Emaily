77c3268081501405a24bc691339ee303eef690dd
Pierre Lambert <supagoof924@hotmail.com>
2019-06-19 14:27:43 +0200

    [ FEATURE ] Added home link

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -13,17 +14,25 @@ class Header extends Component {
           </li>
         );
       default:
-        return <li><a>Logout</a></li>;
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
     }
   }
   render() {
+    console.log(this.props.auth);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
-          <ul className="right">
-            <li>{this.renderContent()}</li>
-          </ul>
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
