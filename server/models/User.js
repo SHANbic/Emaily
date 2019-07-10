54a5ef558f98d72ba79611da8abc7ec00d5a29a9
Pierre Lambert <supagoof924@hotmail.com>
2019-07-10 23:16:17 +0200

    [ CONFIG ] Handled credits update on payment

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,9 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleID: String,
-  name : String,
-  mail : String
+  name: String,
+  mail: String,
+  credits: { type: Number, default: 0 }
 });
 
-mongoose.model('users', userSchema) //REVIEW On indique à la base de données qu'on va utiliser le schéma users
+mongoose.model('users', userSchema); //REVIEW On indique à la base de données qu'on va utiliser le schéma users
