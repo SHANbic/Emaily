8a197cec1a3e01c55cc9c45a2c4d5f44b004e355
Pierre Lambert <supagoof924@hotmail.com>
2019-07-10 22:28:05 +0200

    [ CONFIG ] Added billingRoutes handler

--- server/index.js
+++ server/index.js
@@ -4,8 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User'); //REVIEW on charge le model User
-require('./services/passport');// REVIEW on fait appel à toute notre configuration de passport
-
+require('./services/passport'); // REVIEW on fait appel à toute notre configuration de passport
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true }); // REVIEW  connexion à mongoDB (aussi simple que ça?)
 
 const app = express(); // REVIEW notre app va utiliser express
@@ -21,6 +20,7 @@ app.use(passport.initialize()); //REVIEW on démarre la prise en charge de passp
 app.use(passport.session()); // REVIEW on indique à passport qu'on gère des sessions(notamment avec cookieSession)
 
 require('./routes/authRoutes')(app); // REVIEW on importe les routes gérées par express
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
