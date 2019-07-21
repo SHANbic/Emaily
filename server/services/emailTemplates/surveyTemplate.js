d62f33742ca1f86c5bf9201dd46bbcb46cf2e01d
Pierre Lambert <supagoof924@hotmail.com>
2019-07-21 14:07:19 +0200

    [ FEATURE ] Updated email template

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,19 @@
 module.exports = survey => {
-  return `<div>${survey.body}</div>`
-}
+  return `
+  <html>
+    <body>
+      <div style="text-align: center">
+        <h3>How bad do you love our products?</h3>
+        <p>Your feedback truly matters, please help us getting better</p>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
+};
