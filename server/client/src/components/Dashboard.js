946a7ebeaa6717d7e59b32ae2419de813c45fda2
Pierre Lambert <supagoof924@hotmail.com>
2019-07-22 20:27:55 +0200

    [ FEATURE ] Added add button to create new survey

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div className="fixed-action-btn">
+        <a class="btn-floating btn-large red">
+          <i class="material-icons">add</i>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
