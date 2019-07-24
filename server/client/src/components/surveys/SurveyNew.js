a16100ce62baec43b8aaac425a02d68666cabbd9
Pierre Lambert <supagoof924@hotmail.com>
2019-07-24 22:32:06 +0200

    [ FEATURE ] Added a review form step before final submission

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends React.Component {
-  render() {
+  state = { showFormReview: false };
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    }
+
     return (
-      <div>
-        <h1>Survey new !</h1>
-        <SurveyForm />
-      </div>
+      <SurveyForm
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
+      />
     );
+  }
+
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
