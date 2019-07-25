4e2555f42372b9c9c1ff7b016f955cc27881659f
Pierre Lambert <supagoof924@hotmail.com>
2019-07-25 09:45:41 +0200

    [ FEATURE ] Retrieved data into FormReview from store

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = formFields.map(({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      {reviewFields}
       <button className="yellow darken-3 btn-flat" onClick={onCancel}>
         Back
       </button>
@@ -13,7 +24,6 @@ const SurveyFormReview = ({ onCancel }) => {
 };
 
 function mapStateToProps(state) {
-  console.log(state);
-  return {};
+  return { formValues: state.form.surveyForm.values };
 }
 export default connect(mapStateToProps)(SurveyFormReview);
--- server/client/src/components/surveys/formFields.js
+++ server/client/src/components/surveys/formFields.js
@@ -1,8 +1,6 @@
-const FIELDS = [
+export default [
   { name: 'title', label: 'Survey Title' },
   { name: 'body', label: 'Email Body' },
   { name: 'subject', label: 'Subject Line' },
   { name: 'emails', label: 'Recipient List' }
 ];
-
-export default FIELDS;
