f5cf230c2ec5ef8c1ec2112ce6ff341bea359c83
Pierre Lambert <supagoof924@hotmail.com>
2019-07-25 20:25:10 +0200

    [ FEATURE ] Handled redirection on form submit with withRouter helper

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 import * as actions from '../../actions/index';
+import { withRouter } from 'react-router-dom';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = formFields.map(({ name, label }) => {
     return (
       <div key={name}>
@@ -24,7 +25,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
         Back
       </button>
       <button
-        onClick={() => submitSurvey(formValues)}
+        onClick={() => submitSurvey(formValues, history)}
         className="green white-text btn-flat right"
       >
         Send Survey<i className="material-icons right">email</i>
@@ -39,4 +40,4 @@ function mapStateToProps(state) {
 export default connect(
   mapStateToProps,
   actions
-)(SurveyFormReview);
+)(withRouter(SurveyFormReview));
